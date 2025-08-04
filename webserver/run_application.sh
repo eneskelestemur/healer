79b3de6a013f3eddba36f47d85bfa69ba4f51e7c
Enes Kelestemur <enesk@unc.edu>
2025-08-03 20:13:06 -0400

    removed bash files

--- webserver/run_application.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the directory containing EnumeratorApp.py
-cd /nas/longleaf/home/enesk/enes/enumerator/webserver
-
-# Run the Python web server
-python3 EnumeratorApp.py
